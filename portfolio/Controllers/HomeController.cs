89c030477ae8c6672c189ca1fc5371ec5f0b32c8
shaggy <someaim1@gmail.com>
2024-03-22 18:23:57 +0545

    Completed the frontend part of the basic portfolio

---
+++ portfolio/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+using Microsoft.AspNetCore.Mvc;
+using portfolio.Models;
+using System.Diagnostics;
+
+namespace portfolio.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [Route ("About")]
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        [Route("Skills")]
+        public IActionResult Skills()
+        {
+            return View();
+        }
+
+        [Route("Services")]
+        public IActionResult Services() {
+            return View();
+        }
+
+        [Route("Contact")]
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
